7f004cb4de62c028eb55a0df73f2cd48ed846e3d
Glenn Roberts <me@glenn-roberts.com>
2022-10-24 12:21:09 +1000

    MOAR

--- test/k6/load-test.js
+++ test/k6/load-test.js
@@ -2,21 +2,30 @@ import http from "k6/http";
 
 import { sleep, check } from "k6";
 
-export const options = {
-  cookie: __ENV.LIVEBEATS_COOKIE,
-};
+const cookie = __ENV.LIVEBEATS_COOKIE;
 
 export default function () {
-  const res = http.get("http://localhost:4000/profile/settings", {
+  const options = {
     redirects: 0,
     cookies: {
-      _live_beats_key_v1: options.cookie,
+      _live_beats_key_v1: cookie,
     },
-  });
+  };
+
+  let res = http.get("http://localhost:4000/profile/settings", options);
 
   check(res, {
     "status 200": (r) => r.status === 200,
     "contains header": (r) => r.body.includes("Profile Settings"),
+  });
+
+  sleep(1);
+
+  res = http.get("http://localhost:4000/glennr", options);
+
+  check(res, {
+    "songs status 200": (r) => r.status === 200,
+    "contains table": (r) => r.body.includes("Artist"),
   });
 
   sleep(1);
